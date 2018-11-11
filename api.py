9e2420aa4e031948e18d612a7557e6fc76d6a0b2
Zhunissali Shanabek <zshanabek@gmail.com>
2018-11-11 20:30:40 +0200

    [Refactor] - api

--- api.py
+++ api.py
@@ -7,46 +7,46 @@ app = Flask(__name__)
 api = Api(app)
 
 now = str(datetime.datetime.now())
-
+ok = datetime.datetime.now().strftime('%d.%m.%Y')
 TODOS = [
     {
-        'id': 1,
+        'id': 0,
         'title': 'build an API',
         'description': 'this is desc1',
-        'due_date': now,
+        'due_date': ok,
         'completed': True,
         'created_at': now,
         'updated_at': now
     },
     {
-        'id':2,
+        'id': 1,
         'title': "write article",
         'description': 'this is desc1',
-        'due_date': now,
+        'due_date': ok,
         'completed': True,
         'created_at': now,
         'updated_at': now
     },
     {
-        'id':3,
+        'id': 2,
         'title': 'profit!',
         'description': 'this is desc1',
-        'due_date': now,
+        'due_date': ok,
         'completed': True,
         'created_at': now,
         'updated_at': now
     }
 ]
 
-
 def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+	a = any(d['id'] == todo_id for d in TODOS)
+	if (a == False):
+		abort(404, message="Todo {} doesn't exist".format(todo_id))
 
 parser = reqparse.RequestParser()
-parser.add_argument('title')
+parser.add_argument('title', required=True)
 parser.add_argument('description')
-parser.add_argument('due_date')
+parser.add_argument('due_date', required = True)
 parser.add_argument('completed', type=inputs.boolean)
 parser.add_argument('created_at')
 parser.add_argument('updated_at')
@@ -62,9 +62,11 @@ class Todo(Resource):
         return '', 204
 
     def put(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
         args = parser.parse_args()
         prev = TODOS[todo_id]
         task = {
+                'id': prev['id'],
                 'title': prev['title'] if args['title'] == None else args['title'],
                 'description': prev['description'] if args['description'] == None else args['description'],
                 'due_date': prev['due_date'] if args['due_date'] == None else args['due_date'],
@@ -80,23 +82,23 @@ class TodoList(Resource):
         return TODOS, 200, {'Access-Control-Allow-Origin': '*'}
 
     def post(self):
+        now = str(datetime.datetime.now())
         args = parser.parse_args()
-        todo_id = int(max(TODOS.keys())) + 1
-        todo_id = '%i' % todo_id
-        TODOS[todo_id] = {
+        todo_id = len(TODOS)
+        todo = {
+            'id': todo_id,
             'title': args['title'],
             'description': args['description'],
             'due_date': args['due_date'],
-            'completed': args['completed'],
-            'created_at': args['created_at'],
-            'updated_at': args['updated_at']
-            }
+            'completed': False,
+            'created_at': now,
+            'updated_at': now
+        }
+        TODOS.append(todo)
         return TODOS[todo_id], 201
-    
 
 api.add_resource(TodoList, '/todos', '/todos/')
-api.add_resource(Todo, '/todos/<todo_id>')
-
+api.add_resource(Todo, '/todos/<int:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
